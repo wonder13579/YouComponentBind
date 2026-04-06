d358165cb7cb6d0fe53d5d19ec3b0aa16bc2149f
wonder13579 <1020557973@qq.com>
2026-04-06 18:29:11 +0800

    小删下不需要的函数

--- YouComponentBindUnity/Assets/Example/Scripts/Gen/FirstWindow.cs
+++ YouComponentBindUnity/Assets/Example/Scripts/Gen/FirstWindow.cs
@@ -4,31 +4,6 @@ using UnityEngine;
 // 新增事件后，可用IDE补全IFirstWindowEventFunction接口，添加新事件。
 public partial class FirstWindow : MonoBehaviour, IFirstWindowEventFunction
 {
-    public void OnButtonButtonClick()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnInputInputFieldValueChanged(string value)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnInputInputFieldEndEdit(string value)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnToggleItemValueChanged(bool value)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnToggleToggleValueChanged(bool value)
-    {
-        throw new System.NotImplementedException();
-    }
-
     public void OnButton_ButtonClick()
     {
         throw new System.NotImplementedException();
