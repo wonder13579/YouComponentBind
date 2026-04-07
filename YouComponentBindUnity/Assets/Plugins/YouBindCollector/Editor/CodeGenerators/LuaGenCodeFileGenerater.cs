a6782b5f7068cc1d2254e4a68adef6a9cb0b61e7
wonder13579 <1020557973@qq.com>
2026-04-07 20:48:10 +0800

    删除测试代码

--- YouComponentBindUnity/Assets/Plugins/YouBindCollector/Editor/CodeGenerators/LuaGenCodeFileGenerater.cs
+++ YouComponentBindUnity/Assets/Plugins/YouBindCollector/Editor/CodeGenerators/LuaGenCodeFileGenerater.cs
@@ -19,6 +19,8 @@ namespace YouBindCollector
 
 --- @class @ClassName@
 local @ClassName@ = {}
+UIPanelName = UIPanelName or {}
+UIPanelName.@ClassName@PanelName = ""@ClassName@PanelName""
 local PanelName = UIPanelName.@ClassName@PanelName
 
 UIRegistry.Register(PanelName, @ClassName@)
